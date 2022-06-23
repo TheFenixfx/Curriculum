545eedf21793f9a6d61aad824fd66a9c17c75baa
Jhon <knifefx@gmail.com>
2022-06-23 19:01:40 -0430

    saving

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -430,7 +430,7 @@ jQuery(document).ready(function($) {
             }, 2000);
         });
 */
-        document.getElementById("skillbar-bar");
+        document.getElementsByClassName("skillbar-bar");
          Elem.style.position = "absolute";
          Elem.animate({
             width: ['8px', '280px']

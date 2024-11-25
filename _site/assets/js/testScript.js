722be33e079e8fcabf031a12b0546fb4f9e83195
TheFenixfx <knifefx@gmail.com>
2024-11-25 02:24:44 -0400

    savi

--- _site/assets/js/testScript.js
+++ _site/assets/js/testScript.js
@@ -1,5 +1,5 @@
 
-setTimeout(function() {
+document.addEventListener('DOMContentLoaded', function() {
 const canvas = document.getElementById('background-effect');
 const ctx = canvas.getContext('2d');
 
